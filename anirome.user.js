ede09709fb1f363256361e9b2101095bb7a53deb
Fmohican <fmohican@users.noreply.github.com>
2020-09-14 22:55:32 +0300

    Update anirome.user.js

--- anirome.user.js
+++ anirome.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         AniroME EasyPost Integration
-// @version      0.1
+// @version      0.2
 // @description  You are lazy shit? It's okay cuz i'm.
 // @author       Fmohican
 // @match        https://aniro.me/upload/*
@@ -10,15 +10,21 @@
 // @grant window.focus
 // @grant        GM_*
 // @grant        GM.*
+// @supportURL   https://github.com/fmohican/easypost-api/issues
+// @source       https://github.com/fmohican/easypost-api
+// @updateURL    https://github.com/fmohican/easypost-api/raw/master/anirome.user.js
+// @downloadURL  https://github.com/fmohican/easypost-api/raw/master/anirome.user.js
 // @license      WTFPL
 // @run-at       document-end
 // ==/UserScript==
 
 function btnReload() {
-    $('.upload').prepend("<div class='row'><a href='#' class='btn btn-danger easypost-reload'>[Reload Buttons]</a></div>");
+    $('.upload').prepend("<div class='row' id='btn-reload'><a href='#' class='btn btn-danger easypost-reload'>[Reload Buttons]</a></div>");
     $(document).on('click', '.easypost-reload', function () {
         $('#easypost').remove();
+        $('#btn-reload').remove();
         doButtons(getdata_list());
+        $('.upload').prepend("<div class='row' id='btn-reload'><div class='col-md-6'><a href='#' class='btn btn-danger easypost-reload'>[Reload Buttons]</a></div><div class='col-md-6'>Last Update: "+new Date().toString()+"</div></div>");
     });
 }
 
@@ -36,7 +42,7 @@ function getdata_list() {
 function doButtons(data) {
     $('.upload').prepend("<div class='row' id='easypost' style='margin-top:20px;'></div>");
     $.each(data, function (index, value) {
-        $('#easypost').append("<div class='col-md-1'><a href='#' class='btn btn-primary easypost' data-id='"+value.id+"'>"+value.title+"</a></div>");
+        $('#easypost').append("<div class='col-md-3' style='margin-top:10px;'><a href='#' class='btn btn-primary easypost' data-id='"+value.id+"'>"+cutTitle(value.title)+"</a></div>");
     });
 }
 
@@ -44,6 +50,7 @@ function btnTrigger() {
     $(document).on('click', '.easypost', function () {
         let data = getData($(this).data('id'));
         //$('input#title').val(data.title);
+        $("textarea#meta_decription").val(data.desc);
         $('input#autoimdb').val(data.imdb);
         $('input#autotmdb').val(data.tmdb);
         $('input#tvdb').val(data.tvdb);
@@ -55,6 +62,12 @@ function btnTrigger() {
     });
 }
 
+function cutTitle(input) {
+    if (input.length > 26) {
+      return input.substring(0, 23) + '...';
+   }
+   return input;
+}
 
 function getData(id) {
     let req = $.ajax({
